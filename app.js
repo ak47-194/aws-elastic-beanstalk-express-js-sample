334ead1a8edc308aa43bd7ae5a64507e92d2966a
ak47-194 <120406145+ak47-194@users.noreply.github.com>
2025-08-14 13:36:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const express = require('express');
+const express = require("express");
+const bodyParser= require("body-parser");
 const app = express();
-const port = 8080;
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', (req, res) => res.send('Hello DEVOPS!'));
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+app.get("/about", (req, res) => res.send("Amrit Dash"));
+
+app.get("/contact", (req, res) => res.send("ph no 79xxxxxxxxx"));
+
+app.post("/", function(req, res) {
+  var num1=Number(req.body.n1);
+  var num2=Number(req.body.n2);
+  var result=num1+num2;
+  res.send("The result of your calculation is " + result);
+})
+app.listen(3000, function() {
+  console.log("server started on port 3000");
+});
